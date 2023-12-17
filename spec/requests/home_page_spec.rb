02724cd3970e9dcb8188c1a7c513b83e96eb98c4
Neil Atkinson <yalcneil@gmail.com>
2023-12-17 10:36:30 +0000

    Set root path to show the home page

--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'HomePages', type: :request do
   describe 'GET /show' do
     it 'returns http success' do
-      get '/home_page/show'
+      get '/'
       expect(response).to have_http_status(:success)
     end
   end
