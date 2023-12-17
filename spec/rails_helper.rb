8826e1e0f8389808620ed8a634ee202dc5a530f4
Neilos <yalcneil@gmail.com>
2023-12-17 20:44:43 +0000

    Add url helpers to RSpec DSL

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  config.include Rails.application.routes.url_helpers
   config.include FactoryBot::Syntax::Methods
 
   Shoulda::Matchers.configure do |shoulda_config|
