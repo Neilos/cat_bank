d3eddf4dd1802231e67bfa1fa1d6cf1c90778bf4
Neilos <yalcneil@gmail.com>
2023-12-18 07:16:37 +0000

    Users are awarded 100 silveuros upon sign up

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AccountsController < ApplicationController
+  before_action :ensure_user_account_is_setup
+
   def show
     @account = account
     @money_transaction_items = MoneyTransactionItem
@@ -9,19 +11,47 @@ class AccountsController < ApplicationController
                                .eager_load(:money_transaction)
                                .order(id: :desc)
                                .page params[:page]
+
+    flash.now[:notice] = I18n.t('new_sign_up_award') if new_user?
   end
 
   private
 
   def account
-    existing_account || new_account
+    existing_user_account || new_account
   end
 
-  def existing_account
-    Account.find_by(user: current_user)
+  def new_user?
+    existing_user_account.blank?
+  end
+
+  def existing_user_account
+    return @existing_user_account if defined?(@existing_user_account)
+
+    @existing_user_account = Account.find_by(user: current_user)
+  end
+
+  def ensure_user_account_is_setup
+    return unless new_user?
+
+    MoneyTransactionCreationService.call(
+      description: 'New signup award',
+      money_transaction_items_attributes: [
+        { account_id: system_account.id, amount: -award_amount.to_money(:seu) },
+        { account_id: new_account.id, amount: award_amount.to_money(:seu) }
+      ]
+    )
+  end
+
+  def system_account
+    Account.where(user: nil).sole
   end
 
   def new_account
-    Account.create(user: current_user)
+    @new_account ||= Account.create(user: current_user)
+  end
+
+  def award_amount
+    100.to_money(:seu)
   end
 end
--- spec/system/sign_up_spec.rb
+++ spec/system/sign_up_spec.rb
@@ -3,6 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'signing up' do
+  let(:system_account) { create(:account, user: nil) }
   let(:user_email) { 'test@example.com' }
   let(:user_password) { 'password123!!' }
   let(:sign_up_actions) do
@@ -19,21 +20,45 @@ RSpec.describe 'signing up' do
     end
   end
 
+  before do
+    system_account
+  end
+
   it 'signs the user up, logs them in and redirects to the accounts show page' do
     sign_up_actions.call
 
-    expect(page).to have_text('Welcome! You have signed up successfully')
+    expect(page).to have_text('Welcome.')
     expect(page).to have_current_path(user_account_root_path)
   end
 
   it 'creates the user and an account' do
     expect do
       sign_up_actions.call
-      expect(page).to have_text('Welcome!')
+      expect(page).to have_text('Welcome.')
     end.to(
       change(User, :count).by(1).and(
         change(Account, :count).by(1)
       )
+    )
+  end
+
+  it 'awards the user with 100 Silveuros' do
+    sign_up_actions.call
+    expect(page).to have_text(
+      'Welcome. You have been awarded 100 free Silveuros!'
+    )
+
+    expect(page).to have_current_path(user_account_root_path)
+
+    expect(page).to have_table(
+      'Transactions',
+      with_rows: [
+        {
+          'Description' => 'New signup award',
+          'Amount' => 100.to_money(:seu),
+          'Balance' => 100.to_money(:seu)
+        }
+      ]
     )
   end
 end
